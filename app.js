06fb43bfaa1c6e290da961e5c595271f86660dba
AkyunaAkish <akyunaakish@gmail.com>
2015-07-07 10:45:19 -0600

    initialized repo

---
+++ app.js
@@ -0,0 +1,20 @@
+var http = require('http'),
+    router = require('./router'),
+    url = require('url')
+
+var server = http.createServer(function (req, res) {
+    console.log(req.url);
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, {'Content-Type': 'image/x-icon'})
+        res.end();
+        return;
+    }
+    var path = url.parse(req.url).pathname
+    var currentRoute = router.match(path)
+    currentRoute.fn(req, res, currentRoute)
+})
+
+server.listen(5555, function (err) {
+    if (err) console.log('Doah', err)
+    console.log('Woot. A server is running on port 5555')
+})
---
+++ router.js
@@ -0,0 +1,84 @@
+var routes = require('i40')();
+var fs = require('fs');
+var db = require('monk')('localhost/tutorials');
+var qs = require('qs');
+var tutorials = db.get('tutorials');
+var view = require('./view');
+var mime = require('mime');
+
+routes.addRoute('/', function(req,res,url){
+    res.setHeader('Content-Type', 'text/html');
+    if (req.method === 'GET') {
+        tutorials.find({}, function (err, docs) {
+            var template = view.render('/tutorials/index', {tutorials:docs});
+            res.end(template);
+        })
+    }
+    if (req.method === "POST") {
+        var result = '';
+        req.on('data', function (chunk) {
+            result += chunk;
+        });
+        req.on('end', function () {
+            var tutorial = qs.parse(result);
+            tutorials.insert(tutorial, function (err, doc) {
+                if (err) {
+                    res.end('ERROR!!!!!!!!UZHASNO!');
+                }
+                res.writeHead(302, {'Location': '/'});
+                res.end();
+
+            })
+        })
+    }
+});
+
+routes.addRoute('/public/*', function (req, res, url) {
+    res.setHeader('Content-Type', mime.lookup(req.url));
+    fs.readFile('.' + req.url, function (err, file) {
+        if (err) {
+            res.setHeader('Content-Type', 'text/html');
+            res.write('404')
+        }
+        res.end(file);
+    })
+});
+
+routes.addRoute('/tutorials/:id/edit', function (req, res, url) {
+    url = url.params.id;
+    if (req.method === 'GET') {
+        res.setHeader('Content-Type', 'text/html');
+        tutorials.findOne({_id: url}, function (err, docs) {
+            var template = view.render('tutorials/edit', docs);
+            res.end(template);
+        })
+    }
+});
+
+routes.addRoute('/tutorials/:id/delete', function (req, res, url) {
+    if (req.method === 'POST') {
+        tutorials.remove({_id: url.params.id}, function (err, doc) {
+            if (err) console.log(err);
+            res.writeHead(302, {'Location': '/'});
+            res.end()
+        })
+    }
+});
+
+routes.addRoute('/tutorials/:id/update', function(req, res , url){
+    var data = '';
+    req.on('data', function(chunk){
+        data += chunk;
+    });
+
+    req.on('end', function(){
+        var tutorial = qs.parse(data);
+        tutorial.update({_id: url.params.id}, {topic: tutorial.topic, link: tutorial.link, description: tutorial.description, tags: tutorial.tags}, function(err,doc){
+            res.writeHead(302, {'Location': '/'});
+            res.end();
+        })
+    })
+});
+
+
+module.exports = routes;
